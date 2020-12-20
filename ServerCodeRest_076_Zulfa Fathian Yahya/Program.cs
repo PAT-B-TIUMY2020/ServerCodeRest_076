dddee1acc9360b6811477441925df60e4c0a7a23
fafathian <56532114+fafathian@users.noreply.github.com>
2020-12-20 15:46:16 +0700

    servercode

--- ServerCodeRest_076_Zulfa Fathian Yahya/Program.cs
+++ ServerCodeRest_076_Zulfa Fathian Yahya/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
+using ServiceRest_076_Zulfa_Fathian_Yahya;
 
 namespace ServerCodeRest_076_Zulfa_Fathian_Yahya
 {
@@ -10,6 +14,40 @@ namespace ServerCodeRest_076_Zulfa_Fathian_Yahya
     {
         static void Main(string[] args)
         {
+            ServiceHost hostObjek = null;
+            Uri address = new Uri("http://localhost:1918/Mahasiswa");
+            WebHttpBinding bind = new WebHttpBinding();
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY), address);
+                //ALAMAT BASE ADDRESS
+                hostObjek.AddServiceEndpoint(typeof(ITI_UMY), bind, "");
+
+
+                //ALAMAT ENDPOINT
+                //wsdl
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior(); //Service Runtime Player
+                smb.HttpGetEnabled = true; //untuk mengaktifkan wsdl (dibuka saat development, tidak untuk dibuka)
+                hostObjek.Description.Behaviors.Add(smb);
+                //mex
+                Binding mexbind = MetadataExchangeBindings.CreateMexHttpBinding();
+                hostObjek.AddServiceEndpoint(typeof(IMetadataExchange), mexbind, "mex");
+
+                WebHttpBehavior whb = new WebHttpBehavior();
+                whb.HelpEnabled = true;
+                hostObjek.Description.Endpoints[0].EndpointBehaviors.Add(whb);
+
+                hostObjek.Open();
+                Console.WriteLine("Server is ready!!!!");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
         }
     }
 }
